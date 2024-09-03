0b8ca3c62ae4ad7b45e3e0d73a7f2a8348fb4552
Trinadh Rayala <3nadhmail@gmail.com>
2024-09-03 17:28:25 -0500

    users api added

--- src/main/java/com/trinadh/user_service/UserServiceApplication.java
+++ src/main/java/com/trinadh/user_service/UserServiceApplication.java
@@ -1,8 +1,13 @@
 package com.trinadh.user_service;
 
+import com.trinadh.user_service.dto.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -10,6 +15,13 @@ public class UserServiceApplication {
 	@GetMapping("/greetings")
 	public String greetings(){
 		return "Contgratulations, Successfully deployed application in kubernetes cluster...!";
+	}
+
+	@GetMapping("/users")
+	public List<User> loadUsers(){
+		return Stream.of(new User(1, "Trinadh", "3nadhmail@gmail.com"),
+				new User(2, "Rakesh", "rakeshdatta@gmail.com"),
+				new User(3, "Rayala", "rayalanikendukura@gmail.com")).collect(Collectors.toList());
 	}
 
 	public static void main(String[] args) {
