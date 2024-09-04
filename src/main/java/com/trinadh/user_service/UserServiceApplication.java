e4ae73c6f5fe8b3221d428b976797343d101382c
Trinadh Rayala <3nadhmail@gmail.com>
2024-09-03 19:27:27 -0500

    users api udpated

--- src/main/java/com/trinadh/user_service/UserServiceApplication.java
+++ src/main/java/com/trinadh/user_service/UserServiceApplication.java
@@ -4,12 +4,14 @@ import com.trinadh.user_service.dto.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@RestController
 public class UserServiceApplication {
 
 	@GetMapping("/greetings")
